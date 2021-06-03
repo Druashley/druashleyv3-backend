60ff7146d7ba176a772d3d78c9b564a419c1ebc2
Dru <druashley@gmail.com>
2021-06-02 19:01:07 -0700

    Set up USER routes for my website

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// code for an server logic will go here
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+//Express
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => console.log(`The Server has started on port ${PORT}`));
+
+//Mongoose
+
+mongoose.connect(
+  process.env.MONGODB_CONNECTION_STRING,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  },
+  (err) => {
+    if (err) throw err;
+    console.log("MongoDB connection Established");
+  }
+);
+
+// Routes
+
+app.use("/users", require("./routes/userRouter"));
---
+++ models/userModel.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: { type: String, required: true, minLength: 5 },
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+});
+
+module.exports = User = mongoose.model("user", userSchema);
